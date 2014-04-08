c1a21a9a1fc4f7ab7934a0bb0d40dbb3ecde4cf9
Ian Jamieson <i@njamieson.co.uk>
2014-04-08 18:43:10 +0100

    Adding grunt server task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,29 @@ module.exports = function(grunt) {
         js_dest: 'app/js',
         css_dest: 'app/css'
       }
+    },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          hostname: 'localhost',
+          port: 9001,
+          base: 'app'
+        }
+      }
+    },
+    open : {
+      dev : {
+        path: 'http://localhost:9001/',
+        app: '/Applications/Google Chrome.app'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-bower');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+
+  grunt.registerTask('server', ['connect', 'open:dev']);
 
 };
